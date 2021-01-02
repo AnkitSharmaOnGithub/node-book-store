9e644c9e53d3fc5736b6cfda875be9468e5b70c2
Ankit Sharma <ankitsharma101299@gmail.com>
2021-01-02 21:16:44 +0530

    Update

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 // Importing Node Native Modules
 const http = require("http");
-const path = require('path');
+const path = require("path");
 
 // Importing 3rd Party Modules
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require("./models/tasks_model");
+
+db.execute("SELECT * FROM tasks").then((data) => {
+  console.log(data);
+});
 
 // Setting Project Initials
 const app = express();
-app.set('view engine','pug');
-app.set('views','views');
+app.set("view engine", "pug");
+app.set("views", "views");
 
 // Setting Routes and static files Path
-const routes = require('./routes/tasks_routes');
-app.use(express.static(path.join(__dirname,'/public')));
+const routes = require("./routes/tasks_routes");
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(routes);
 
 const server = http.createServer(app);
