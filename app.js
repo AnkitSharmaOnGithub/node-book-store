32398e0b42e36279c6558a01ce4cd4d5c973aaee
Ankit Sharma <ankitsharma101299@gmail.com>
2021-01-02 20:10:10 +0530

    Added template engine

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const http = require("http");
+const path = require('path');
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(req.method);
-  next();
-});
+app.set('view engine','pug');
+app.set('views','views');
+app.use(express.static(path.join(__dirname,'/public')));
 
-app.use((req, res, next) => {
-  res.send("From 2nd");
+
+app.use('/',(req, res, next) => {
+  res.render('index');
 });
 
 const server = http.createServer(app);
