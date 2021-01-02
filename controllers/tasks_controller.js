2e37126384d7fe3c8124ea6a37cc4fbdfdc85ffc
Ankit Sharma <ankitsharma101299@gmail.com>
2021-01-02 21:12:22 +0530

    Update

--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -2,6 +2,18 @@ exports.getBooks = (req, res, next) => {
   res.render("index");
 };
 
-exports.addBooks = (req, res, next) => {
+exports.addBook = (req, res, next) => {
   res.render('add_task');
+};
+
+exports.postAddBook = (req,res,next) => {
+  console.log(req.body);
+}
+
+exports.getBook = (req, res, next) => {
+  res.render('view_book');
+};
+
+exports.editBook = (req, res, next) => {
+  res.render('edit_book');
 };
