d0ebfc97552c591160742be0c310949ecfb74866
Ankit Sharma <ankitsharma101299@gmail.com>
2021-01-03 12:38:37 +0530

    Updated

--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -1,19 +1,48 @@
+const TasksModel = require("../models/tasks_model");
+
 exports.getBooks = (req, res, next) => {
   res.render("index");
 };
 
 exports.addBook = (req, res, next) => {
-  res.render('add_task');
+  res.render("add_task");
 };
 
-exports.postAddBook = (req,res,next) => {
-  console.log(req.body);
-}
+exports.postAddBook = (req, res, next) => {
+  const taskName = req.body.taskName;
+  const taskDesc = req.body.taskDesc;
 
-exports.getBook = (req, res, next) => {
-  res.render('view_book');
+  const task = new TasksModel(taskName, taskDesc);
+  task
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.viewTasks = (req, res, next) => {
+  TasksModel.fetchAll().then(([rows,fields]) => {
+    console.log(rows);
+    res.render("view_tasks", {
+      tasks: rows,
+      pageTitle: "View Books",
+    });
+  });
+};
+
+exports.viewTask = (req, res, next) => {
+  const id = req.params.id;
+  TasksModel.findById(id).then(([rows,fields]) => {
+    console.log(rows);
+    res.render('view_task', {
+      tasks : rows
+    })
+  })
 };
 
 exports.editBook = (req, res, next) => {
-  res.render('edit_book');
+  res.render("edit_book");
 };
