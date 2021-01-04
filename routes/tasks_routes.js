dff49805e3b74039152e3a3d368deffc518c8ff9
Ankit Sharma <ankitsharma101299@gmail.com>
2021-01-04 11:45:01 +0530

    Update

--- routes/tasks_routes.js
+++ routes/tasks_routes.js
@@ -5,14 +5,20 @@ const router = express.Router();
 const tasksController = require('../controllers/tasks_controller');
 
 
-router.get("/add", tasksController.addBook);
+router.get("/add", tasksController.addTask); // Handles Add a Task [GET REQUEST]
 
-router.post("/add", tasksController.postAddBook);
+router.post("/add", tasksController.postAddTask);  // Handles Add a Task [POST REQUEST]
 
-router.get("/view", tasksController.viewTasks);
+router.get("/view", tasksController.viewTasks);  //  Handles View All Tasks request
 
-router.get("/view/:id", tasksController.viewTask);
+router.get("/view/:id", tasksController.viewTask);  // Handles View Single Task request
 
-router.get("/", tasksController.getBooks);
+router.get("/delete/:id", tasksController.deleteTask);  // Handles Delete Task request
+
+router.get("/edit/:id", tasksController.editTask);  // Handles Edit Task GET request
+
+router.post("/edit/:id", tasksController.postEditTask);  // Handles Edit Task POST request
+
+router.get("/", tasksController.getTasks); // Handles Home Page request
 
 module.exports = router;
