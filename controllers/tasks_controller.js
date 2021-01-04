4ccb4749615deb99f30e5f5c44369e6a53ed6022
Ankit Sharma <ankitsharma101299@gmail.com>
2021-01-04 11:45:16 +0530

    Update

--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -1,33 +1,40 @@
 const TasksModel = require("../models/tasks_model");
+const JSAlert = require("js-alert");
 
-exports.getBooks = (req, res, next) => {
+//  Handles View All Books request
+exports.getTasks = (req, res, next) => {
   res.render("index");
 };
 
-exports.addBook = (req, res, next) => {
+//  Handles Add a book [GET REQUEST]
+exports.addTask = (req, res, next) => {
   res.render("add_task");
 };
 
-exports.postAddBook = (req, res, next) => {
+// Handles Add a book [POST REQUEST]
+exports.postAddTask = (req, res, next) => {
   const taskName = req.body.taskName;
   const taskDesc = req.body.taskDesc;
 
-  const task = new TasksModel(taskName, taskDesc);
-  task
-    .save()
-    .then(() => {
-      res.redirect("/");
+  TasksModel.create({
+    task_name: taskName,
+    task_desc: taskDesc,
+  })
+    .then((data) => {
+      console.log(data);
+      res.redirect("/view/");
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
+// Handles Add a book [POST REQUEST]
 exports.viewTasks = (req, res, next) => {
-  TasksModel.fetchAll().then(([rows,fields]) => {
-    console.log(rows);
+  TasksModel.findAll().then((tasks) => {
+    // console.log(tasks);
     res.render("view_tasks", {
-      tasks: rows,
+      tasks: tasks,
       pageTitle: "View Books",
     });
   });
@@ -35,14 +42,55 @@ exports.viewTasks = (req, res, next) => {
 
 exports.viewTask = (req, res, next) => {
   const id = req.params.id;
-  TasksModel.findById(id).then(([rows,fields]) => {
-    console.log(rows);
-    res.render('view_task', {
-      tasks : rows
+  TasksModel.findAll({ where: { task_id: id } })
+    .then((product) => {
+      res.render("view_task", {
+        tasks: product,
+      });
     })
-  })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
-exports.editBook = (req, res, next) => {
-  res.render("edit_book");
+exports.deleteTask = (req, res, next) => {
+  const id = req.params.id;
+
+  TasksModel.findByPk(id)
+  .then(product => {
+    return product.destroy();
+  })
+  .then(result => {
+    console.log('Deleting Product');
+    res.redirect('/view');
+  });
+};
+
+exports.editTask = (req, res, next) => {
+  const id = req.params.id;
+  TasksModel.findAll({ where: { task_id: id } }).then((product) => {
+    res.render("edit_task", {
+      tasks: product,
+    });
+  });
+};
+
+exports.postEditTask = (req, res, next) => {
+  const title = req.body.taskName;
+  const desc = req.body.taskDesc;
+  const id = req.params.id;
+
+  TasksModel.findByPk(id)
+    .then(task => {
+      task.task_name = title;
+      task.task_desc = desc;
+      return task.save();
+    })
+    .then((result) => {
+      console.log('Updated Task');
+      res.redirect('/view');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
